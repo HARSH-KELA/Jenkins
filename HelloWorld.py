6c6866db94618da00eb1e6ee8ba33580b7e22eba
HARSH-KELA <137981959+HARSH-KELA@users.noreply.github.com>
2024-02-01 09:43:42 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,7 +2,7 @@
 # This Phython program will print Hellow World...
 print("Hello World Demo...Jan 2024...\n")
 print("Hello World Demo...Jan 2024...\n")
-print("Hell...Jan 2024...\n")
+print("Hel...Jan 2024...\n")
 
 
 
